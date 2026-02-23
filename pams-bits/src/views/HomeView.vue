f138773aca5069c16cf64cc6e97bea4b7cc27a44
Caroline Young <Cyoung29@ycp.edu>
2026-02-23 11:02:52 -0500

    Changed up the content as per pams request

--- pams-bits/src/views/HomeView.vue
+++ pams-bits/src/views/HomeView.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home-container">
     <header class="hero">
-      <h1>Quality Gear for Quality Rides</h1>
-      <p>Expertly fitted to your horse's unique needs.</p>
+      <h1>Quality Bits and Bridles</h1>
+      <h1>Fit with Care</h1>
       <router-link to="/services" class="cta-button">View Our Services</router-link>
     </header>
 
@@ -13,9 +13,9 @@
         </div>
         <div class="text-box">
           <h2>Meet Pam</h2>
-          <p style="margin-bottom: 20px;">A lifelong horse person with a deep-rooted passion for the equestrian world, Pam currently manages a diverse herd of five horses across multiple disciplines, primarily Dressage and Eventing, on her three-generation family farm. She blends her extensive hands-on experience with a highly analytical background, holding a BS in Systems Engineering and an MS in Epidemiology. Her unique perspective allows her to approach bitting not just as a craft, but as a science.</p>
+          <p style="margin-bottom: 20px;">A lifelong horse person with a deep-rooted passion for the equestrian world, Pam currently manages a diverse herd of five horses across multiple disciplines, primarily Dressage and Eventing, on her three-generation family farm. She just recently received her USDF Bronze medal with a horse she trained exclusively. She blends her extensive hands-on experience with a highly analytical background, holding a BS in Systems Engineering and an MS in Epidemiology. Her unique perspective allows her to approach bitting not just as a craft, but as a science.</p>
 
-        <p style="margin-bottom: 20px;">Pam is at the forefront of equine connection science, having earned her Qualified Bit Analyst Certification from the Neue Schule Academy in November 2025. Her academic credentials in the field are equally distinguished; she graduated with Distinction in both Equine Bits & Bitting Science and Lorinery Science. Currently, Pam is further refining her expertise as she works toward her QualtFitter Certification, expected in January 2026.</p>
+        <p style="margin-bottom: 20px;">Pam is at the forefront of equine connection science, having earned her Qualified Bit Analyst Certification from the Neue Schule Academy in November 2025. Her academic credentials in the field are equally distinguished. She graduated with Distinction in both Equine Bits & Bitting Science and Lorinery Science.</p>
         </div>
       </section>
 
@@ -25,13 +25,13 @@
         </div>
         <div class="text-box">
           <h2>Our Mission</h2>
-          <p style="margin-bottom: 20px;">What is Bits and Bridles about? It's about safety, style, and the perfect fit.</p>
+          <p style="margin-bottom: 20px;">What is Bits and Bridles fitting about? It's about safety, style, and the perfect fit.</p>
 
-           <p style="margin-bottom: 20px;">Often we struggle when we are riding - searching  for that right feel of connection with our horse.  We often are very careful to have our saddles fit by a professional fitter - but how often do we look to our bridle and bit for what’s missing in the connection?</p> 
+           <p style="margin-bottom: 20px;">Often we struggle when we are riding - searching  for that right feel of connection with our horse.  We often are very careful to have our saddles fit by a professional fitter - but how often do we look to our bit or bridle for what’s missing in the connection?</p> 
 
           <p style="margin-bottom: 20px;">Regardless of your level of riding, your horse will perform better for you if he is comfortable in his bit and bridle.  Is your bridle something that needs adjustment - or do you need a different style to accommodate a specific type of head?  Is he comfortable in his bit that you are using?  Do you need to try a different size or different bit action altogether?</p>
 
-           <p style="margin-bottom: 20px;">There are so many modern types of bits that have been researched and designed by companies specifically to address certain needs of horse and rider.  I am here to help you navigate that journey and find the best possible combination of bit and bridle that makes your horse comfortable and you as effective a rider as you can be. </p>
+           <p style="margin-bottom: 20px;">There are so many modern types of bits that have been researched and designed by companies specifically to address certain needs of horse and rider.  Pam is here to help you navigate that journey and find the best possible combination of bit and bridle that makes your horse comfortable and you as effective a rider as you can be. </p>
           <router-link to="/booking" class="cta-button">Have Questions?</router-link>
         </div>
       </section>
